cd1bcf665ff69a5792f5e155c752431469cad6e9
Antonio Ishac <antonioishac@gmail.com>
2025-04-06 20:21:33 -0300

    New project files

---
+++ src/test/java/br/com/cactus/cactussensors/device_management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cactus.cactussensors.device_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
