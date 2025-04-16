e0cbfaeebd95778881d56c8a634e7135777747de
Antonio Ishac <antonioishac@gmail.com>
2025-04-16 19:44:49 -0300

    New project files

--- src/main/java/br/com/cactus/cactussensors/device/management/Application.java
+++ src/main/java/br/com/cactus/cactussensors/device/management/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeviceManagementApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeviceManagementApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
